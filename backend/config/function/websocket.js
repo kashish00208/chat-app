10465733f58b47f2f26cd5d3f718b8c22319ec1f
kashish00208 <kgupta7827@gmail.com>
2025-02-15 04:34:45 +0530

    need nextjs here

--- backend/config/function/websocket.js
+++ backend/config/function/websocket.js
@@ -1,14 +1,6 @@
-const socketIo = require("socket.io");
-module.exports = async (strapi) {
-    const server = strapi.server;
-    const io = socketIo(server.httpServer,{cors:{origin:'*'}});
-    socket.on('connectio',(message)=>{
-        console.log("Message from client",message)
-
-        socketIo.emit("message",message)
-    });
-    socketIo.on("disconnect",()=>{
-        console.log("User disconnected");
+module.exports = async(strapi){
+    const io = require('socket.io')(strapi.server.httpServer,{
+        cors:
     })
-    
+
 }
